22eeb96fac98b1c26483756c3e6fb2411244dd6e
Andreas Heige <a.heige.83@gmail.com>
2020-12-22 14:22:13 +0100

    added static items in room

--- pa-2d-game/src/scenes/KitchenScene.tsx
+++ pa-2d-game/src/scenes/KitchenScene.tsx
@@ -6,23 +6,39 @@ import Interactable from '../@core/Interactable';
 import ScenePortal from '../@core/ScenePortal';
 import Sprite from '../@core/Sprite';
 import TileMap, { TileMapResolver } from '../@core/TileMap';
-import { mapDataString } from '../@core/utils/mapUtils';
+import {
+    mapDataString,
+    insertRandomMarks,
+    insertNRandomMarks,
+} from '../@core/utils/mapUtils';
 import CoffeeMachine from '../entities/CoffeeMachine';
 import PizzaPickup from '../entities/PizzaPickup';
 import Plant from '../entities/Plant';
 import Player from '../entities/Player';
-import Workstation from '../entities/Workstation';
 import Refrigerator from '../entities/Refrigerator';
+import Apple from '../entities/food/apple';
+import Bacon from '../entities/food/bacon';
+import Banana from '../entities/food/banana';
+import Bread from '../entities/food/bread';
+import Butter from '../entities/food/butter';
+import Candy from '../entities/food/candy';
+import Cheese from '../entities/food/cheese';
+import Cucumber from '../entities/food/cucumber';
+import Egg from '../entities/food/egg';
+import Onion from '../entities/food/onion';
+import Sausage from '../entities/food/sausage';
+import Tomato from '../entities/food/tomato';
+import Watermelon from '../entities/food/watermelon';
 import spriteData from '../spriteData';
 
 const mapData = mapDataString(`
 # # # # # # # # # # # # # # # # #
-# · · · · · · · · · · · · · R R #
-# · · · · · · · · · · · · · · · #
-# · · · · · · B · · · · · # # # #
-· · · · · · · · · · · · · C # # #
-# · · · · · · · · · · · · · · · #
-# · · · · · · · · · · · · · · · #
+# i · · · · · l · · · · · · R R #
+# · · a · B · · · e  f· · · · · #
+# · · · · · · · · · · · · # # # #
+· · · · · b · c · · · · · C # # #
+# · · · · · · d · · g · · · · m #
+# j · k · · · · · · · · h · · · #
 # # # # # # # # # # # # # # # # #
 `);
 
@@ -32,7 +48,7 @@ const resolveMapTile: TileMapResolver = (type, x, y) => {
 
     const floor = (
         <GameObject key={key} {...position} layer="ground">
-            <Sprite {...spriteData.objects} state="floor3" />
+            <Sprite {...spriteData.objects} state="floor2" />
         </GameObject>
     );
 
@@ -52,13 +68,6 @@ const resolveMapTile: TileMapResolver = (type, x, y) => {
                     <Collider />
                     <Sprite {...spriteData.objects} state="wall3" />
                 </GameObject>
-            );
-        case 'W':
-            return (
-                <Fragment key={key}>
-                    {floor}
-                    <Workstation {...position} />
-                </Fragment>
             );
         case 'C':
             return (
@@ -86,6 +95,97 @@ const resolveMapTile: TileMapResolver = (type, x, y) => {
                 <Fragment key={key}>
                     {floor}
                     <Bat {...position} />
+                </Fragment>
+            );
+        case 'a':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Apple {...position} />
+                </Fragment>
+            );
+        case 'b':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Bacon {...position} />
+                </Fragment>
+            );
+        case 'c':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Banana {...position} />
+                </Fragment>
+            );
+        case 'd':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Bread {...position} />
+                </Fragment>
+            );
+        case 'e':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Butter {...position} />
+                </Fragment>
+            );
+        case 'f':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Candy {...position} />
+                </Fragment>
+            );
+        case 'g':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Cheese {...position} />
+                </Fragment>
+            );
+        case 'h':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Cucumber {...position} />
+                </Fragment>
+            );
+        case 'i':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Egg {...position} />
+                </Fragment>
+            );
+        case 'j':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Onion {...position} />
+                </Fragment>
+            );
+        case 'k':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Sausage {...position} />
+                </Fragment>
+            );
+        case 'l':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Tomato {...position} />
+                </Fragment>
+            );
+        case 'm':
+            return (
+                <Fragment key={key}>
+                    {floor}
+                    <Watermelon {...position} />
                 </Fragment>
             );
         default:
