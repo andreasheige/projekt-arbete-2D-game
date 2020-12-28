96574f72554ae84549bc2278e81568340d2d6682
mikearson <mikael.karlsson9012@gmail.com>
2020-12-28 13:13:32 +0100

    TriggerScript

--- pa-2d-game/src/entities/Rat.tsx
+++ pa-2d-game/src/entities/Rat.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import tileUtils from '../@core/utils/tileUtils';
-import Collider from '../@core/Collider';
+import Collider, { TriggerEvent } from '../@core/Collider';
 import GameObject, { GameObjectProps } from '../@core/GameObject';
 import Sprite from '../@core/Sprite';
 import spriteData from '../spriteData';
+import useGame from '../@core/useGame';
 import useGameObject from '../@core/useGameObject';
+import useGameObjectEvent from '../@core/useGameObjectEvent';
 import Moveable, { MoveableRef } from '../@core/Moveable';
 import useGameLoop from '../@core/useGameLoop';
-import useCollisionTest from '../@core/useCollisionTest';
 import CharacterScript from '../components/CharacterScript';
+import RunningAwayScript from '../components/RunningAwayScript';
+import { useSound } from '../@core/Sound';
+import soundData from '../soundData';
 
 // prettier-ignore
 function RatScript() {
@@ -57,6 +61,27 @@ function RatScript() {
     return null;
 }
 
+function TriggerScript() {
+    const { publish } = useGame();
+    const { getRef } = useGameObject();
+    const playSfx = useSound(soundData.eating);
+
+    async function sendChangeScoreNotification() {
+        await publish('CHANGE_SCORE', 10);
+    }
+
+    useGameObjectEvent<TriggerEvent>('trigger', other => {
+        if (other.name === 'player') {
+            // TODO: signal score loss
+            sendChangeScoreNotification();
+            getRef().setDisabled(true);
+            playSfx();
+        }
+    });
+
+    return null;
+}
+
 export default function Rat(props: GameObjectProps) {
     return (
         <GameObject layer="obstacle" {...props}>
@@ -65,7 +90,8 @@ export default function Rat(props: GameObjectProps) {
             <CharacterScript>
                 <Sprite {...spriteData.rat} state="rat" />
             </CharacterScript>
-            <RatScript />
+            <RunningAwayScript reactionSpeed={500} />
+            <TriggerScript />
         </GameObject>
     );
 }
