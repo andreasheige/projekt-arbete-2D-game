aa6263e633739103f7e42b76a7ae2a5fd19b9945
Andreas Heige <a.heige.83@gmail.com>
2020-12-30 10:21:25 +0100

    Changed so player dtarts in hallway

--- pa-2d-game/src/App.tsx
+++ pa-2d-game/src/App.tsx
@@ -41,7 +41,7 @@ export default function App() {
                 <ScoreProvider>
                     <Game cameraZoom={80}>
                         <AssetLoader urls={urls} placeholder="Loading assets ...">
-                            <SceneManager defaultScene="kitchen">
+                            <SceneManager defaultScene="hallway">
                                 <Scene id="hallway">
                                     <HallwayScene />
                                 </Scene>
