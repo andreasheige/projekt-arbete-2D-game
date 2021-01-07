9aafeb302f83ff5a5edd68a89c038970c180b0ba
Andreas Heige <54802517+andreasheige@users.noreply.github.com>
2021-01-07 10:35:08 +0100

    styled listview (#53)

--- pa-2d-game/src/components/HighScoreListPage.tsx
+++ pa-2d-game/src/components/HighScoreListPage.tsx
@@ -43,10 +43,97 @@ export default function HighScoreListPage({ endingLevel }: Props) {
         </li>
     ));
     return (
-        <div>
-            <h1>Game Over</h1>
-            List of Results
-            <ul>{resultList}</ul>
+        <div
+            id="wrapper"
+            style={{
+                backgroundColor: '#292B37',
+                margin: '15, 20, 20, 15',
+                paddingLeft: '17px',
+                paddingRight: '17px',
+                width: '850px',
+                height: '650px',
+                borderRadius: '25px',
+            }}
+        >
+            <div id="header">
+                <h1
+                    style={{
+                        color: '#F0242C',
+                        paddingLeft: '35%',
+                        paddingRight: 'auto',
+                        fontWeight: 'bold',
+                        textShadow: '1px 2px 3px white',
+                        fontSize: '3rem',
+                    }}
+                >
+                    Game Over
+                </h1>
+            </div>
+            <div
+                id="list"
+                style={{
+                    color: '#F4F1EA',
+                    height: '400px',
+                    marginBottom: '5px',
+                }}
+            >
+                <h2
+                    style={{
+                        color: '#F0242C',
+                        fontFamily: 'arial',
+                        textTransform: 'uppercase',
+                    }}
+                >
+                    List of Results
+                </h2>
+                <ol
+                    style={{
+                        color: '#F0242C',
+                        fontFamily: 'arial',
+                        textTransform: 'uppercase',
+                    }}
+                >
+                    {resultList}
+                </ol>
+            </div>
+            <div
+                id="footer"
+                style={{
+                    position: 'relative',
+                    bottom: '0px',
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                }}
+            >
+                <a
+                    style={{
+                        color: '#292B37',
+                        padding: '15px 15px 15px 15px',
+                        fontFamily: 'arial',
+                        textDecoration: 'none',
+                        textTransform: 'uppercase',
+                        backgroundColor: '#F4F1EA',
+                        borderRadius: '90px',
+                    }}
+                    href="https://krokslottet-test.s3.eu-north-1.amazonaws.com/index.html"
+                >
+                    spela igen
+                </a>
+                <a
+                    style={{
+                        color: '#292B37',
+                        padding: '15px 15px 15px 15px',
+                        fontFamily: 'arial',
+                        textDecoration: 'none',
+                        textTransform: 'uppercase',
+                        backgroundColor: '#F4F1EA',
+                        borderRadius: '90px',
+                    }}
+                    href="https://andreasheige.me/krakslottet/index.html"
+                >
+                    startsida
+                </a>
+            </div>
         </div>
     );
 }
