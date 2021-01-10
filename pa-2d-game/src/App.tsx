7dc97d2f126eaf1ef7379c7faf03278910a61e20
karlglans <karl_glans@hotmail.com>
2021-01-10 13:49:33 +0100

    decrease trash room 3

--- pa-2d-game/src/App.tsx
+++ pa-2d-game/src/App.tsx
@@ -42,7 +42,7 @@ export default function App() {
                 <ScoreProvider>
                     <Game cameraZoom={80}>
                         <AssetLoader urls={urls} placeholder="Loading assets ...">
-                            <SceneManager defaultScene="garden">
+                            <SceneManager defaultScene="study">
                                 <Scene id="hallway">
                                     <HallwayScene />
                                 </Scene>
